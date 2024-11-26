fa0f072a7fda69c90f9a0dc3da129fd00396537f
Dabs <paulandreiardiente@gmail.com>
2024-11-26 22:16:47 +0800

    [Paul] Added a header and also created some bootstrap elements to be used in the code

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Header = () => {
+  return (
+    <button type="button" className='btn btn-danger position-absolute top-3 end-3'>LOG OUT</button>
+  )
+}
+
+export default Header

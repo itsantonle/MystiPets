74f58ed22ed54b2749f21ed5c906a77fd97513fd
Prince Herrera <princeherreracpu@gmail.com>
2024-11-26 18:54:55 +0800

    feat: add Supabase connection in connect.ts

--- server/src/db/connect.ts
+++ server/src/db/connect.ts
@@ -1,13 +1,17 @@
-import { Pool } from "pg"
-import dotenv from "dotenv"
-dotenv.config()
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: Number(process.env.DBPORT),
-  database: "database",
-})
+  user: process.env.PGUSER, 
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT || 5432),
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
 
-export default pool
+export default pool;

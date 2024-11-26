f3a58562ba7925d8003e1d9dc2cd3ed30ca94a89
Dabs <paulandreiardiente@gmail.com>
2024-11-26 09:33:45 +0800

    [Paul] added petContainer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import { Stage, Container } from "@pixi/react"
 import Hydra from "./components/Hydra"
 import Capycorn from "./components/Capycorn"
+// import PetContainer from "./components/PetContainer"
 
 const App = () => {
   return (
     <Stage
       width={800}
       height={600}
-      options={{ background: 0xD83D }}
+      options={{ background: 0xD83D  }}
     >
+      {/* <PetContainer type  */}
       <Container x={160} y={200}>
         <Capycorn x={220} y={80}/>
         <Hydra x={220} y={140}/>
---
+++ client/src/components/PetContainer.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// const PetContainer = (props) => {
+//     {pettype="Hydra"| "Capybara"}
+//     const {pettype, petxposition} = props 
+//     {switch(pettype){
+//         case "capybara": dfjasd; break
+//         <Capybara x= oet
+//         case "hydra ": dfsdfsd 
+
+
+//     }}
+    
+//   return (
+//     <div>PetContainer</div>
+//   )
+// }
+
+// export default PetContainer

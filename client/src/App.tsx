bb8c2b5f5e654a9da83d92c1fe3793a643d07d9f
Justine <justinemarie.altar-18@cpu.edu.ph>
2024-11-26 11:09:31 +0800

    [Justine] Moddified Pet Container size method.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,8 +11,8 @@ const App = () => {
     >
       {/* <PetContainer type  */}
       <Container x={160} y={200}>
-        <PetContainer petType ="Hydra" petSize={450} petXpos={100} petYpos={100}/>
-        <PetContainer petType ="Capycorn" petSize={225} petXpos={160} petYpos={200}/>
+        <PetContainer petType ="Hydra" petXpos={100} petYpos={100}/>
+        <PetContainer petType ="Capycorn" petXpos={160} petYpos={200}/>
       </Container>
     </Stage>
   )
--- client/src/components/PetContainer.tsx
+++ client/src/components/PetContainer.tsx
@@ -1,12 +1,13 @@
 import Hydra from "./pets/Hydra"
 import Capycorn from "./pets/Capycorn"
 
-export type petConfig = {petType: string, petXpos: number, petYpos: number, petSize: number}
+export type petConfig = {petType: string, petXpos: number, petYpos: number}
+const petSize = 384
 
-const PetContainer = ({ petType, petXpos, petYpos, petSize}: petConfig) => {
+const PetContainer = ({ petType, petXpos, petYpos}: petConfig) => {
     {switch(petType){
         case "Capycorn":
-            return <Capycorn s={petSize} x={petXpos} y={petYpos}/>
+            return <Capycorn s={petSize} x={petXpos} y={petYpos}/>;
         case "Hydra":
             return <Hydra s={petSize} x={petXpos} y={petYpos}/>;
     }}

6b0b77f065f7a93e2d1b9083dc890d2500f1a0a5
JP Rosalita <jprosalita14@gmail.com>
2024-11-29 10:31:29 +0800

    [JP] styling changes and modified assets

--- client/src/components/InteractivePanel.tsx
+++ client/src/components/InteractivePanel.tsx
@@ -1,5 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { manageHappiness } from '../utils/interfaceUtil/happinessBarUtil';
+import happyStar from '../components/img/icons/happy_star.png';
+import meat from '../components/img/icons/meat.png';
+import healthBar from '../components/img/icons/health-bar1.png'
+import feedButton from '../components/img/icons/feedButton.png'
+import playButton from '../components/img/icons/playButton.png'
 
 const Panel = () => {
     const happinessValue = ''
@@ -8,23 +13,26 @@ const Panel = () => {
     return (
         <div className = "panel-container position-absolute top-80 start-50 translate-middle">
             <div className = "counter-container">
-                <div className = "counter-style"> &#9825;: {happinessValue} </div>
-                <div className = "counter-style"> &#167;: {hungerValue} </div>
+                <div className = "counter-style"> <img src={happyStar} className="img-fluid" />: {happinessValue} </div>
+                <div className = "counter-style"> <img src={meat} className="img-fluid" />: {hungerValue} </div>
             </div>
 
             <div className="name-bar-button-container">
                 <div className="name-buttons-container">
                     <textarea className = "name-text-style" placeholder="Pet Name" readOnly></textarea>
 
-                    <button className = "button-style" type = "button" onClick={manageHappiness}>P</button>
-                    <button className = "button-style" type = "button" onClick={manageHappiness}>F</button>
-                    <button className = "button-style" type = "button" onClick={manageHappiness}>S</button>
+                    <button className = "button-style" type = "button" onClick={manageHappiness}><img src={feedButton} className="img-fluid"/></button>
+                    <button className = "button-style" type = "button" onClick={manageHappiness}><img src={playButton} className="img-fluid"/></button>
+                    <button className = "button-style" type = "button" onClick={manageHappiness}><img src={playButton} className="img-fluid"/></button>
                 </div>
-                <div className="health-bar-style">  ============Health Bar Here==========</div>
+                <div className="health-bar-style"> 
+                    <textarea className = "HP-text-style" placeholder="HP" readOnly></textarea> 
+                    <img src={healthBar} className="img-fluid"/>
+                </div>
             </div>
 
             <div className="mood-container">
-                <textarea className = "mood-style" placeholder="The quick brown Fox jumps over the lazy Dog." readOnly></textarea>
+                <textarea className = "mood-style" placeholder="putangina ninyong lahat!" readOnly></textarea>
             </div>
             
         </div>

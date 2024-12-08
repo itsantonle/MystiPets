d4d039d7ef2e31e1f981897c68e2dc14c6f6359a
A. Legayada <tonilegayada@gmail.com>
2024-12-08 23:22:24 +0800

    pre desctructuion fix
    fix the missiing images and file

---
+++ client/src/components/pets/Capycorn.tsx
@@ -0,0 +1,33 @@
+import { AnimatedSprite } from "@pixi/react"
+import {Capycorn_Idle} from "../img/Animations"
+
+export type dimensions = { x: number; y: number; s: number }
+
+const CapyChooser = () => {
+  
+}
+
+const CapycornSprite = ({ x, y, s}: dimensions) => {
+  
+    
+  return (
+    <>
+      <AnimatedSprite
+        anchor={0.5}
+        textures={Capycorn_Idle}
+        isPlaying={true}
+        initialFrame={0}
+        animationSpeed={0.07}
+        interactive
+        x={x} 
+        y={y}
+        width={s} 
+        height={s}
+      />
+
+    </>
+  )
+}
+
+
+export default CapycornSprite

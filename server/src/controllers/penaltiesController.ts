cece1d7cb87fbc683bb7bb4a929590fcf57fa8f0
Prince Herrera <princeherreracpu@gmail.com>
2024-12-08 17:25:25 +0800

    finish penalty routing and controller for post, put and delete requests

--- server/src/controllers/penaltiesController.ts
+++ server/src/controllers/penaltiesController.ts
@@ -3,42 +3,104 @@ import pool from "../db/connect"
 import { StatusCodes } from "http-status-codes"
 import {
   internalErrorResponse,
-  sendError,
   successResponse,
+  successUpdateResponse,
+  sucessfulDeleteResponse,
+  successPostResponse,
+  unsucessfulDeleteResponse,
+  unsucessfulPostResponse,
+  unsucessfulUpdateResponse,
 } from "../utils/errorHandling"
-import { sendNotFoundError } from "../utils/errorHandling"
 
-export const getAllPenalties = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
+export const getPenalties = async (req: Request, res: Response): Promise<void> => {
   try {
-    const response = await pool.query("SELECT * FROM penalty")
-    res.status(StatusCodes.OK).json(successResponse(response.rows))
+    const response = await pool.query("SELECT * FROM penalty");
+    res.status(StatusCodes.OK).json(successResponse(response.rows));
   } catch (error) {
     console.error(error)
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse())
   }
-}
+};
 
-export const getOnePenalty = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
-  const { id } = req.params
+export const createPenalty = async (req: Request, res: Response): Promise<void> => {
+  const { penalty_id, penalty_type, penalty_duration, penalty_description } = req.body;
   try {
     const response = await pool.query(
-      "SELECT * FROM penalty WHERE penalty_id = $1",
-      [id],
-    )
-    res.status(StatusCodes.OK).json(successResponse(response.rows[0]))
+      `INSERT INTO penalty (penalty_id, penalty_type, penalty_duration, penalty_description)
+      VALUES ($1, $2, $3, $4)`,
+      [penalty_id, penalty_type, penalty_duration, penalty_description]
+    );
+    res.status(StatusCodes.CREATED).json(successPostResponse(response.rows[0]));
   } catch (error) {
-    console.error(error)
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json(internalErrorResponse())
+    console.error(error);
+    res.status(StatusCodes.BAD_REQUEST).json(unsucessfulPostResponse());
   }
-}
-// you should change this to penalty_type instead of penalty id
+};
+
+export const updatePenaltyType = async (req: Request, res: Response): Promise<void> => {
+  const { penalty_id } = req.params;
+  const { penalty_type } = req.body;
+  try {
+    const response = await pool.query(
+      `UPDATE penalty
+      SET penalty_type = $1
+      WHERE penalty_id = $2`,
+      [penalty_type, penalty_id]
+    );
+    res.status(StatusCodes.OK).json(successUpdateResponse(response.rows[0]));
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.BAD_REQUEST).json(unsucessfulUpdateResponse());
+  }
+};
+
+export const updatePenaltyDuration = async (req: Request, res: Response): Promise<void> => {
+  const { penalty_id } = req.params;
+  const { penalty_duration } = req.body;
+  try {
+    const response = await pool.query(
+      `
+      UPDATE penalty
+      SET penalty_duration = $1
+      WHERE penalty_id = $2 RETURNING *`,
+      [penalty_duration, penalty_id]
+    );
+    res.status(StatusCodes.OK).json(successUpdateResponse(response.rows[0]));
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.BAD_REQUEST).json(unsucessfulUpdateResponse());
+  }
+};
+
+export const updatePenaltyDescription = async (req: Request, res: Response): Promise<void> => {
+  const { penalty_id } = req.params;
+  const { penalty_description } = req.body;
+  try {
+    const response = await pool.query(
+      `
+      UPDATE penalty
+      SET penalty_description = $1
+      WHERE penalty_id = $2 RETURNING *`,
+      [penalty_description, penalty_id]
+    );
+    res.status(StatusCodes.OK).json(successUpdateResponse(response.rows[0]));
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.BAD_REQUEST).json(unsucessfulUpdateResponse());
+  }
+};
+
+export const deletePenalty = async (req: Request, res: Response): Promise<void> => {
+  const { penalty_id } = req.params;
+  try {
+    const response = await pool.query("DELETE FROM penalty WHERE penalty_id = $1", [penalty_id]);
+
+    res.status(StatusCodes.OK).json(sucessfulDeleteResponse());
+    
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.BAD_REQUEST).json(unsucessfulDeleteResponse());
+  }
+};
--- server/src/routes/penaltiesRoute.ts
+++ server/src/routes/penaltiesRoute.ts
@@ -1,13 +1,20 @@
 import { 
-  getAllPenalties, 
-  getOnePenalty
+  getPenalties,
+  createPenalty,
+  updatePenaltyType,
+  updatePenaltyDuration,
+  updatePenaltyDescription,
+  deletePenalty,
 } from "../controllers/penaltiesController";
 import express from "express";
 
 const router = express.Router();
 
-router.route("/").get(getAllPenalties)
-
-router.route("/:id").get(getOnePenalty);
+router.route("/getPenalties").get(getPenalties);
+router.route("/createPenalty").post(createPenalty);
+router.route("/updatePenaltyType/:penalty_id").put(updatePenaltyType); 
+router.route("/updatePenaltyDuration/:penalty_id").put(updatePenaltyDuration); 
+router.route("/updatePenaltyDescription/:penalty_id").put(updatePenaltyDescription); 
+router.route("/deletePenalty/:penalty_id").delete(deletePenalty); 
 
 export default router;

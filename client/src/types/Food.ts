08584e17c3916a1e8e0f2621b75b29124b137e3c
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2024-12-09 22:58:39 +0800

    added more api

---
+++ client/src/types/Food.ts
@@ -0,0 +1,8 @@
+type Food = {
+    food_name: string
+    food_type: string
+    food_description: string
+    food_id: number
+}
+
+export default Food
---
+++ client/src/types/Penalty.ts
@@ -0,0 +1,8 @@
+type Penalty = {
+    penalty_type: string
+    penalty_description: string
+    penalty_duration: number
+    penalty_id: number
+}
+
+export default Penalty

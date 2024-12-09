5851089f9975ba2735773f28cdbff1d40cd808ea
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2024-12-09 14:39:10 +0800

    added penaltyAPI

--- server/src/routes/penaltiesRoute.ts
+++ server/src/routes/penaltiesRoute.ts
@@ -15,9 +15,9 @@ const router = express.Router();
 router.route("/getPenalties/:player_uuid").get(getPenalties);
 router.route("/createPenalty").post(createPenalty);
 router.route("/assignPenalty/:player_uuid").put(assignPenalty);
-router.route("/updatePenaltyType/:player_uuid").put(updatePenaltyType); 
-router.route("/updatePenaltyDuration/:player_uuid").put(updatePenaltyDuration); 
-router.route("/updatePenaltyDescription/:player_uuid").put(updatePenaltyDescription); 
+router.route("/updatePenaltyType/:penalty_id").put(updatePenaltyType); 
+router.route("/updatePenaltyDuration/:penalty_id").put(updatePenaltyDuration); 
+router.route("/updatePenaltyDescription/:penalty_id").put(updatePenaltyDescription); 
 router.route("/deletePlayerPenalty/:player_uuid").delete(deletePlayerPenalty); 
 router.route("/deletePenalty/:penalty_id").delete(deletePenalty); 
 

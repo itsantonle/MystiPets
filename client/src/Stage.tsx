0271e42af092c9eec7b939eeae88ff59b7b515d6
A. Legayada <tonilegayada@gmail.com>
2024-12-10 01:16:33 +0800

    working data retrieval

--- client/src/Stage.tsx
+++ client/src/Stage.tsx
@@ -2,26 +2,40 @@ import { Stage, Container } from "@pixi/react"
 import Header from "./components/Header"
 import "./bootstrap/bootstrapSass.scss"
 import PetContainer from "./components/PetContainer"
-import * as React from 'react';
+import * as React from "react"
 import Panel from "./components/InteractivePanel"
+import { Pet } from "./types/Pet"
 // import styles from "./components/ui/Styles"
-
-const PetStage: React.FC = () => {
+type PetContainerProp = {
+  pet: Pet
+}
+const PetStage = ({ pet }: PetContainerProp) => {
   return (
     <>
-    <Header />
-    <Container x={160} y={200} >
-      <Stage width={800} height={475} options={{ background: 0xD83D }}>
+      <Header />
+      <Stage
+        width={800}
+        height={475}
+        options={{ background: 0xd83d }}
+      >
         <Container x={160} y={200}>
-          <PetContainer petType="Hydra" petXpos={100} petYpos={50} />
-          <PetContainer petType="Capycorn" petXpos={400} petYpos={50} />
+          <PetContainer
+            petType={pet.pet_type}
+            petXpos={230}
+            petYpos={pet.pet_type == "Hydra" ? 20 : 10}
+          />
+          {/* <PetContainer
+            petType="Capycorn"
+            petXpos={400}
+            petYpos={50}
+          /> */}
         </Container>
       </Stage>
-    </Container>
-    < Panel />
-    {/* <Footer/> */}
-    </>
-  );
-};
 
-export default PetStage;
+      <Panel />
+      {/* <Footer/> */}
+    </>
+  )
+}
+
+export default PetStage
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "../context/AuthContext"
 
 const Header = () => {
   const { signOut } = useAuth()
+
   return (
     <button
       onClick={signOut}

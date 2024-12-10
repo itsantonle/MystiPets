112e6cc19a1bfe58ffec3313cfabcaa4061ece4b
A. Legayada <tonilegayada@gmail.com>
2024-12-10 12:04:37 +0800

    display name and working intro
    - working intro
    - use hooks with tanstack

--- client/src/components/InteractivePanel.tsx
+++ client/src/components/InteractivePanel.tsx
@@ -1,73 +1,114 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { manageHappiness } from '../utils/interfaceUtil/happinessBarUtil';
-import happyStar from '../components/img/icons/happy_star.png';
-import meat from '../components/img/icons/meat.png';
-import feedButton from '../components/img/icons/feedButton.png';
-import playButton from '../components/img/icons/playButton.png';
-import { getValues } from './DB_PanelLink';
-import { manageHunger } from '../utils/interfaceUtil/hungerBarUtil';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { manageHappiness } from "../utils/interfaceUtil/happinessBarUtil"
+import happyStar from "../components/img/icons/happy_star.png"
+import meat from "../components/img/icons/meat.png"
+import feedButton from "../components/img/icons/feedButton.png"
+import playButton from "../components/img/icons/playButton.png"
+import { getValues } from "./DB_PanelLink"
+import { manageHunger } from "../utils/interfaceUtil/hungerBarUtil"
 import healthBarFrame from "./img/icons/health-bar-frame-1.png"
-import {AnimatedHealthBar} from './healthbar'
+import { AnimatedHealthBar } from "./healthbar"
 // import * as React from "react"
-import {manageHealth} from '../utils/interfaceUtil/healthBarUtil';
+import { manageHealth } from "../utils/interfaceUtil/healthBarUtil"
+import { usePets } from "../services/queries/petQueries"
+import { useAuth } from "../context/AuthContext"
 
 const Panel = () => {
-    // //Uncomment if reall DB is connected
-    // const { happyValue, hungerValue, healthValue, error, loading } = getValues();
+  const { user } = useAuth()
+  const pet = usePets(user!.id).data![0]
 
-    const { isHappyValue, isPlayingClicked } = manageHappiness();
-    const { isHungryValue, isEatingClicked } = manageHunger();
-    const {isHealthyValue, trackIncrease} = manageHealth();
+  // //Uncomment if reall DB is connected
+  // const { happyValue, hungerValue, healthValue, error, loading } = getValues();
 
-    const eatingUtils = () => {
-        isEatingClicked();
-        trackIncrease();
-    }
+  const { isHappyValue, isPlayingClicked } = manageHappiness()
+  const { isHungryValue, isEatingClicked } = manageHunger()
+  const { isHealthyValue, trackIncrease } = manageHealth()
 
-    const playingutils = () => {
-        isPlayingClicked();
-        trackIncrease();
-    }
+  const eatingUtils = () => {
+    isEatingClicked()
+    trackIncrease()
+  }
 
-    return (
-        <div className = "panel-container position-absolute top-80 start-50 translate-middle">
-            <div className = "counter-container">
-                {/* Delete after DB implementation -------------*/} 
-                <div className = "counter-style"> <img src={happyStar} className="img-fluid" />: {isHappyValue} </div>
-                <div className = "counter-style"> <img src={meat} className="img-fluid" />: {isHungryValue} </div>
-                {/* --------------------------------- */}
+  const playingutils = () => {
+    isPlayingClicked()
+    trackIncrease()
+  }
 
-
-                {/* Uncomment if real DB is connected */}
-                {/* <div className = "counter-style"> <img src={happyStar} className="img-fluid" />: {loading ? 'Loading...' : error ? `Error: ${error}` :isHappyValue} </div>
-                <div className = "counter-style"> <img src={meat} className="img-fluid" />: {loading ? 'Loading...' : error ? `Error: ${error}` : isHungryValue} </div> */}
-                
-            </div>
-
-            <div className="name-bar-button-container">
-                <div className="name-buttons-container">
-                    <textarea className = "name-text-style" placeholder="Pet Name" readOnly></textarea>
-
-                    <button className = "button-style" type = "button" onClick={eatingUtils}><img src={feedButton} className="img-fluid"/></button>
-                    <button className = "button-style" type = "button" onClick={playingutils}><img src={playButton} className="img-fluid"/></button>
-                    <button className = "button-style" type = "button" onClick={playingutils}><img src={playButton} className="img-fluid"/></button>
-                </div>
-                <div className="health-bar-style"> 
-                    {isHealthyValue}
-                    <textarea className = "HP-text-style" placeholder="HP:" readOnly></textarea>
-                    <div>
-                        <img src = {healthBarFrame} className="health-bar-frame"/>
-                        <AnimatedHealthBar/>
-                    </div>
-                </div>
-            </div>
-
-            <div className="mood-container">
-                <textarea className = "mood-style" placeholder="Mood" readOnly></textarea>
-            </div>
-            
+  return (
+    <div className="panel-container position-absolute top-80 start-50 translate-middle">
+      <div className="counter-container">
+        {/* Delete after DB implementation -------------*/}
+        <div className="counter-style">
+          {" "}
+          <img src={happyStar} className="img-fluid" />:{" "}
+          {isHappyValue}{" "}
         </div>
-    )
+        <div className="counter-style">
+          {" "}
+          <img src={meat} className="img-fluid" />: {isHungryValue}{" "}
+        </div>
+        {/* --------------------------------- */}
+
+        {/* Uncomment if real DB is connected */}
+        {/* <div className = "counter-style"> <img src={happyStar} className="img-fluid" />: {loading ? 'Loading...' : error ? `Error: ${error}` :isHappyValue} </div>
+                <div className = "counter-style"> <img src={meat} className="img-fluid" />: {loading ? 'Loading...' : error ? `Error: ${error}` : isHungryValue} </div> */}
+      </div>
+
+      <div className="name-bar-button-container">
+        <div className="name-buttons-container">
+          <textarea
+            className="name-text-style"
+            placeholder="pet"
+            readOnly
+          >
+            {pet.pet_name}
+          </textarea>
+
+          <button
+            className="button-style"
+            type="button"
+            onClick={eatingUtils}
+          >
+            <img src={feedButton} className="img-fluid" />
+          </button>
+          <button
+            className="button-style"
+            type="button"
+            onClick={playingutils}
+          >
+            <img src={playButton} className="img-fluid" />
+          </button>
+          <button
+            className="button-style"
+            type="button"
+            onClick={playingutils}
+          >
+            <img src={playButton} className="img-fluid" />
+          </button>
+        </div>
+        <div className="health-bar-style">
+          {isHealthyValue}
+          <textarea
+            className="HP-text-style"
+            placeholder="HP:"
+            readOnly
+          ></textarea>
+          <div>
+            <img src={healthBarFrame} className="health-bar-frame" />
+            <AnimatedHealthBar />
+          </div>
+        </div>
+      </div>
+
+      <div className="mood-container">
+        <textarea
+          className="mood-style"
+          placeholder="Mood"
+          readOnly
+        ></textarea>
+      </div>
+    </div>
+  )
 }
 
-export default Panel;
+export default Panel
--- client/src/types/Pet.ts
+++ client/src/types/Pet.ts
@@ -1,13 +1,13 @@
 export type Pet = {
-  pet_id: number
+  pet_id?: number
   pet_name: string
-  pet_type: "Hydra" | "Capybara"
-  hunger_status: number
-  happiness_status: number
-  is_dead: boolean
-  has_left: boolean
-  health: number
+  pet_type: "Hydra" | "Capycorn"
+  hunger_status?: number
+  happiness_status?: number
+  is_dead?: boolean
+  has_left?: boolean
+  health?: number
   food_id?: 1
   mood_id?: number
-  player_uuid?: string
+  player_uuid: string
 }

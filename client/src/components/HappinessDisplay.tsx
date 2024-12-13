3ee5078eb80387057bbd871fc9a4421c06a6bf29
Kenneth Joel Urbano <kenneth.urbano0127@gmail.com>
2024-12-14 02:46:54 +0800

    added base work for the leave penalty

--- client/src/components/HappinessDisplay.tsx
+++ client/src/components/HappinessDisplay.tsx
@@ -3,20 +3,36 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {
   useUpdateHappiness,
+  useUpdateLeaving,
   useUpdatePetMood,
 } from "../services/mutations/petmutations"
 import { usePets } from "../services/queries/petQueries"
 import { useAuth } from "../context/AuthContext"
 import { useEffect } from "react"
 import happyStar from "../components/img/icons/happy_star.png"
+import { useAssignPenalty } from "../services/mutations/penaltymutations"
 
 export const HappinessDisplay = () => {
   const { user } = useAuth()
   const pet = usePets(user!.id).data![0]
   const updateHappinessMutation = useUpdateHappiness()
   const updateMoodMutation = useUpdatePetMood()
+  const applyPenalty = useAssignPenalty()
+  const updateLeavingStatus = useUpdateLeaving()
 
   useEffect(() => {
+    if (pet.happiness_status === 0 && !pet.has_left) {
+      applyPenalty.mutate({
+        player_uuid: user!.id,
+        player_penalty: 2
+      })
+      updateLeavingStatus.mutate({
+        player_uuid: user!.id,
+        has_left: true
+      })
+
+      window.alert("YOUR PET HAS LEFT YOU")
+    }
     if (pet.happiness_status! > 0 && pet.happiness_status! <= 100) {
       if (
         !updateHappinessMutation.isPending ||
